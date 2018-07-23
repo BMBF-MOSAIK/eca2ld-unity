a01cc233390bf753b837280099d188b8be57ee4d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-07-23 09:29:53 +0200

    Added LD Entity Script

---
+++ LDEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LDEntity : MonoBehaviour
+{
+
+    LDPEntity entity;
+    public string EntityName;
+    // Use this for initialization
+    void Start()
+    {
+        entity = new LDPEntity(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

9c1ea548e39d64b447657fcc7a375f69df5e6739
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-07-23 09:06:15 +0200

    Added TTL Serializer Class to schedule serializing Tasks with Unity thread model

---
+++ TTLSerializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+namespace Assets.Scripts.ECA2LD
+{
+    public class TTLSerializer : MonoBehaviour
+    {
+
+        protected Queue<Action> pendingActions;
+
+        // Use this for initialization
+        void Start()
+        {
+            pendingActions = new Queue<Action>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            lock (pendingActions)
+            {
+                if (pendingActions.Count > 0)
+                {
+                    var action = pendingActions.Dequeue();
+                    action.Invoke();
+                }
+            }
+        }
+
+        public void SerializeTTL(LDPGraph graph, HttpListenerContext c)
+        {
+            byte[] responseBuffer;
+
+            lock (pendingActions)
+            {
+                pendingActions.Enqueue(() =>
+                {
+                    responseBuffer = System.Text.Encoding.UTF8.GetBytes(graph.GetTTL());
+                    c.Response.OutputStream.Write(responseBuffer, 0, responseBuffer.Length);
+                    c.Response.OutputStream.Flush();
+                    c.Response.OutputStream.Close();
+                });
+            }
+        }
+    }
+}

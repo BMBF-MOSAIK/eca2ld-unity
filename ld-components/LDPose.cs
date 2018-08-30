6224ada6c5dbd6ecbbbb3d0d8192125fe62f0af9
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-30 14:03:53 +0200

    Added Pose component which publishes Transform to LD Graph

---
+++ ld-components/LDPose.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.eca2ld_unity.ld_components
+{
+    public class LDPose : LDComponent
+    {
+        [IsLD]
+        public Vector3 Position;
+
+        [IsLD]
+        public Quaternion Orientation;
+
+        public void Update()
+        {
+            Position = gameObject.transform.position;
+            Orientation = gameObject.transform.rotation;
+        }
+    }
+}

ea35ffa639a66e2e7126d01e357eba23b6614442
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-31 10:28:11 +0200

    Added LDPAttribute Datapoint Class

---
+++ ldp-datapoints/LDPAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.eca2ld_unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.ECA2LD
+{
+    class LDPAttribute : LinkedDataPoint
+    {
+        private LDAttribute a;
+
+        public LDPAttribute(GameObject gameObject, LDAttribute a) : base(gameObject)
+        {
+            this.a = a;
+            Name = a.Name;
+            string uri = "http://"
+                + gameObject.GetComponent<LDEntity>().HostAddress + ":"
+                + gameObject.GetComponent<LDEntity>().HostPort + "/"
+                + gameObject.GetComponent<LDEntity>().EntityName + "/"
+                + a.ParentComponent.GetType() + "/" + a.Name + "/";
+            LDPGraph = new AttributeLDPGraph(new Uri(uri), a);
+            initializeHttpListener(uri);
+        }
+
+    }
+}

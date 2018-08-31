5ab2ecd7d7d0ad949faca01bd3e32de6036dff3a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-31 10:27:57 +0200

    LDAttribute now contains all information needed to create datapoint

--- LDAttribute.cs
+++ LDAttribute.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Assets.Scripts.eca2ld_unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assets.Scripts.eca2ld_unity
+namespace Assets.Scripts.ECA2LD
 {
     public class LDAttribute
     {
         public string Name { get; set; }
-        public string Value { get; set; }
+        public object Value { get; set; }
+        public LDComponent ParentComponent { get; private set; }
+
+        public LDAttribute(LDComponent parentComponent, string name, object value)
+        {
+            ParentComponent = parentComponent;
+            Name = name;
+            Value = value;
+        }
     }
 }

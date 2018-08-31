f7ecc8201cd965888687e8d26a8ffbf452ce042e
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-31 10:28:46 +0200

    Linked Data Components now create Attribute Datapoints

--- ldp-datapoints/LDPComponent.cs
+++ ldp-datapoints/LDPComponent.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assets.Scripts.eca2ld_unity;
+using Assets.Scripts.eca2ld_unity.ld_components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,12 @@ namespace Assets.Scripts.ECA2LD
 {
     class LDPComponent : LinkedDataPoint
     {
-        private Component c;
+        private LDComponent c;
+        private List<LDPAttribute> attributes = new List<LDPAttribute>();
 
         public LDPComponent(GameObject gameObject, Component c) : base(gameObject)
         {
-            this.c = c;
+            this.c = c as LDComponent;
             Name = c.GetType().ToString();
             string uri = "http://"
                 + gameObject.GetComponent<LDEntity>().HostAddress + ":"
@@ -23,7 +26,20 @@ namespace Assets.Scripts.ECA2LD
                 + c.GetType() + "/";
             LDPGraph = new ComponentLDPGraph(new Uri(uri), c);
             initializeHttpListener(uri);
+            CreateAttributeDatapoints();
         }
 
+        private void CreateAttributeDatapoints()
+        {
+            foreach (var f in c.GetType().GetFields())
+            {
+                var attr = (IsLDAttribute[])f.GetCustomAttributes(typeof(IsLDAttribute), false);
+                if (attr.Length > 0)
+                {
+                    attributes.Add(new LDPAttribute(gameObject, new LDAttribute(c, f.Name, f.GetValue(c))));
+                }
+            }
+
+        }
     }
 }

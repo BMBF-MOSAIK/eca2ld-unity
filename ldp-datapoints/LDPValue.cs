b63c1f41750fcb2a1f3a705e4a46bb7a5290afd4
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-31 12:23:11 +0200

    Capitalized Uri as it is now a public get property

--- ldp-datapoints/LDPValue.cs
+++ ldp-datapoints/LDPValue.cs
@@ -17,13 +17,13 @@ public class LDPValue : LinkedDataPoint
     public LDPValue(GameObject gameObject, LDAttribute a) : base(gameObject)
     {
         var e = gameObject.GetComponent<LDEntity>();
-        uri = "http://" +
+        Uri = "http://" +
             e.HostAddress + ":" +
             e.HostPort + "/" +
             e.EntityName + "/" +
             a.ParentComponent.GetType() + "/" +
             a.Name + "/value/";
-        LDPGraph = new ValueGraph(new Uri(uri), a);
-        initializeHttpListener(uri);
+        LDPGraph = new ValueGraph(new Uri(Uri), a);
+        initializeHttpListener(Uri);
     }
 }

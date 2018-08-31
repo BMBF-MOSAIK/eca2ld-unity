0948b57f79be2330c74c62b8aa3e730a60e0ed01
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2018-08-31 11:05:32 +0200

    Added Value Datapoint

---
+++ ldp-datapoints/LDPValue.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.ECA2LD;
+using Assets.Scripts.eca2ld_unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using VDS.RDF;
+
+public class LDPValue : LinkedDataPoint
+{
+
+    private LDAttribute a;
+
+    public LDPValue(GameObject gameObject, LDAttribute a) : base(gameObject)
+    {
+        var e = gameObject.GetComponent<LDEntity>();
+        uri = "http://" +
+            e.HostAddress + ":" +
+            e.HostPort + "/" +
+            e.EntityName + "/" +
+            a.ParentComponent.GetType() + "/" +
+            a.Name + "/value/";
+        LDPGraph = new ValueGraph(new Uri(uri), a);
+        initializeHttpListener(uri);
+    }
+}
---
+++ ldp-graphs/ValueGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.ECA2LD
+{
+    public class ValueGraph : LDPGraph
+    {
+        public LDAttribute a { get; private set; }
+        public ValueGraph(Uri u, LDAttribute a) : base(u)
+        {
+            this.a = a;
+        }
+
+        protected override void BuildRDFGraph()
+        {
+            // nothing to do here, as Graph will just work as accessor to attribute, which is admittedly not very beautiful,
+            // but helps to cope with Unity Threading behaviour, as by this, we can just re-use the HttpListener that we wrote
+            // before.
+        }
+    }
+}
